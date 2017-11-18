532e7bccc74e40c19b47fea120c7078ab6a8953e
Corina Hebristean <hebricorina@gmail.com>
2017-11-18 15:07:21 +0200

    create bookings, rooms, accommodations

---
+++ app/Http/Controllers/AccommodationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Accommodation;
+
+class AccommodationController extends Controller
+{
+    public function index ()
+    {
+        $accommodations = Accommodation::all();
+
+        //dd($accommodations); echivalet pentru var_dump();
+
+        return view('accommodations.index', ['accommodations' => $accommodations]);
+    }
+
+
+}
---
+++ resources/views/bookings/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Bookings</h1>
+
+   {{ dd($bookings) }}
+
+@endsection
